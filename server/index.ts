94c5d69bb0fa013a2b040eac4e5eba65ba559165
Ivan Medvedovskyi <ivanmedvedovskiyq@gmail.com>
2025-05-01 15:25:07 +0300

    fix: remove secret from code

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,10 @@ app.get("/", async (request, reply) => {
 
 const start = async () => {
   try {
-    await app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
+    await app.listen({
+      port: Number(process.env.PORT) || 3000,
+      host: "0.0.0.0",
+    });
     console.log(
       `🚀 Server ready at http://localhost:${process.env.PORT || 3000}`
     );
