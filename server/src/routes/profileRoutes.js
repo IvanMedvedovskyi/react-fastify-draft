55c2767a613056b46a35fc4d57e02cc85da110a8
Ivan Medvedovskyi <ivanmedvedovskiyq@gmail.com>
2025-05-02 22:56:28 +0300

    feat: implemented new routes

--- server/src/routes/profileRoutes.js
+++ server/src/routes/profileRoutes.js
@@ -1,7 +1,13 @@
-import { getProfile } from "../controllers/profileController.js";
+import {
+  getProfile,
+  getAllProfiles,
+  updateProfile,
+} from "../controllers/profileController.js";
 
 async function profileRoutes(app, options) {
   app.get("/profile", getProfile);
+  app.get("/getAllProfiles", getAllProfiles);
+  app.put("/updateProfile", updateProfile);
 }
 
 export default profileRoutes;
