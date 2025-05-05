5960052144d1b24fffe5a87173a623c0ec82dc46
Ivan Medvedovskyi <ivanmedvedovskiyq@gmail.com>
2025-05-05 22:19:23 +0300

    fix:fixed errors

--- client/src/app/store/useCharactersStore.ts
+++ client/src/app/store/useCharactersStore.ts
@@ -1,30 +1,14 @@
-import { create } from 'zustand';
-
-export interface Character {
-    id: string;
-    code: string;
-    rank: number;
-    type: number;
-    element: number;
-    camp: number;
-    en: string;
-    ru: string;
-    portrait: string;
-    icon: string;
-    halfPortrait: string;
-    halfPortrait170: string;
-    iconHoyo: string;
-    _id: string;
-}
+import { create } from "zustand";
+import { Character } from "./useProfileStore";
 
 interface CharactersState {
-    characters: Character[];
-    setCharacters: (users: Character[]) => void;
-    clearCharacters: () => void;
+  characters: Character[];
+  setCharacters: (users: Character[]) => void;
+  clearCharacters: () => void;
 }
 
 export const useCharactersStore = create<CharactersState>((set) => ({
-    characters: [],
-    setCharacters: (characters) => set({ characters }),
-    clearCharacters: () => set({ characters: [] }),
+  characters: [],
+  setCharacters: (characters) => set({ characters }),
+  clearCharacters: () => set({ characters: [] }),
 }));
